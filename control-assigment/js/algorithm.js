8c1c60df905fae94de4c206e3273b6c53ebaffa0
annetslx <ardolgopolova@edu.hse.ru>
2025-10-14 22:10:35 +0300

    Added request in algorithm.js

--- control-assigment/js/algorithm.js
+++ control-assigment/js/algorithm.js
@@ -1,14 +1,30 @@
 function calc() {
-    let x = parseFloat(document.getElementById('x').value);
-    let y = parseFloat(document.getElementById('y').value);
+    const problem = "Определить значение функции Z = 1/(XY) при произвольных X и Y";
+    const x = parseFloat(document.getElementById('x').value);
+    const y = parseFloat(document.getElementById('y').value);
 
     if (x === 0 || y === 0 || isNaN(x) || isNaN(y)) {
         document.getElementById('result').innerText = "Ошибка: X и Y должны быть ненулевыми числами!";
         return;
     }
 
-    let z = 1 / (x * y);
+    const z = 1 / (x * y);
     document.getElementById('result').innerText = `Z = ${z.toFixed(4)}`;
+
+    const data = {
+        problem: problem,
+        x: x,
+        y: y,
+        z: z
+    };
+
+    fetch('https://httpbin.org/post', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    });
 }
 
 function goBack() {
